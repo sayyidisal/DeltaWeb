7332ad9b7b6210d9eb5cb9c0e00180fdc63a44c2
方威 <fangwei@fangweideMacBook.local>
2016-01-03 15:27:09 -0700

    ajax form

--- UIDesign/Algorithm_platform/js/index.js
+++ UIDesign/Algorithm_platform/js/index.js
@@ -1,6 +1,23 @@
 (function() {
 	var app = angular.module('platform-index', []);
 
+
+	userInfo = {
+		loginAs: 0, //0 as not login, 1 as student, 2 as instructor
+		username: "Wei",
+		errors:{
+			errorName: true,
+			errorEmail: true,
+			errorAuthen: true,
+		}
+	};
+
+	// $http.get('/user.json').success(function(data) {
+	// 	// pointer changes, can't use this anymore. thus use store to refer to the previous pointer
+	// 	store.user = data;
+	// 	// rest things to do
+	// })
+
 	app.directive('navBar', [function() {
 		return {
 			restrict: 'E',
@@ -16,24 +33,76 @@
 			}],
 		};
 	}])
-	app.controller('FuncCtrl', ['$http', '$log', function($http, $log) {
-		var user = this;
-		// user.info = {
-		// 		loginAs: 0, //0 as not login, 1 as student, 2 as instructor
-		// 		username: "Wei",
-		// }
-		$http.get('/user.json').success(function(data) {
-			// pointer changes, can't use this anymore. thus use store to refer to the previous pointer
-			store.user = data;
-			// rest things to do
-		})
+
+	app.controller('FuncCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+		$rootScope.info = userInfo;
 	}])
 
-	app.controller('SignupCtrl', ['$scope', function($scope) {
+	app.controller('SignupCtrl', ['$http', '$scope', '$rootScope', function($http, $scope, $rootScope) {
+		$scope.formData = {};
+		$scope.errorName = false;
+		$scope.errorEmail = false;
+
 		$scope.submitForm = function() {
+			$http({
+					method: 'POST',
+					url: 'signupProcess', // to be changed
+					data: $.param($scope.formData), // pass in data as strings
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded'
+					} // set the headers so angular passing info as form data (not request payload)
+				})
+				.success(function(data) {
 
-			// check to make sure the form is completely valid
+					if (!data.success) {
+						// if not successful, bind errors to error variables
+						$scope.errorName = data.errors.name; //true if user has been taken
+						$scope.errorEmail = data.errors.email; //true if email has been registered
+						// pass true, error message will show accordingly
 
+					} else {
+						// if success, update user data and apply
+						$rootScope.info = data;
+						$rootScope.$digest();
+						$(function() {
+							$('#signupModal').modal('hide');
+						})
+					}
+				});
+		};
+
+	}])
+
+	app.controller('LoginCtrl', ['$http', '$scope', '$rootScope', function($http, $scope, $rootScope) {
+
+		$scope.formData = {};
+		$scope.errorAuthen = false;
+
+		$scope.submitForm = function() {
+			$http({
+					method: 'POST',
+					url: 'LoginProcess', // to be changed
+					data: $.param($scope.formData), // pass in data as strings
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded'
+					} // set the headers so angular passing info as form data (not request payload)
+				})
+				.success(function(data) {
+
+					if (!data.success) {
+						// if not successful, bind errors to error variables
+						$scope.errorAuthen = data.errors.errorAuthen; //true not match
+	
+					} else {
+						// if success, update user data and apply
+						$rootScope.info = data;
+						$rootScope.$digest();
+						$(function() {
+							$('#loginModal').modal('hide');
+						})
+					}
+
+				});
 		};
 
 	}])
