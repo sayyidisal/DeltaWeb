2d11d3202301d1db432b9d0927df3707817227fc
方威 <fangwei@fangweideMacBook.local>
2016-01-02 22:29:15 -0700

    login/sign up form ready. use get to fetch the data

--- UIDesign/Algorithm_platform/js/index.js
+++ UIDesign/Algorithm_platform/js/index.js
@@ -1,5 +1,5 @@
 (function() {
-	var app = angular.module('platform-list', []);
+	var app = angular.module('platform-index', []);
 
 	app.directive('navBar', [function() {
 		return {
@@ -16,7 +16,23 @@
 			}],
 		};
 	}])
-	
+
+	app.controller('FuncCtrl', ['$scope', function($scope) {
+		this.info = {
+			loginAs: 0, //0 as not login, 1 as student, 2 as instructor
+			username: "Wei",
+		}
+	}])
+
+	app.controller('SignupCtrl', ['$scope', function($scope) {
+		$scope.submitForm = function() {
+
+			// check to make sure the form is completely valid
+
+		};
+
+	}])
+
 })()
 
 jQuery(document).ready(function($) {
