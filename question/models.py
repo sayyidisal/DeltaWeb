45d6f15e6a70531b2781c534075dc3314a1ec974
steve xu <stevexucityu@gmail.com>
2016-01-17 22:12:54 -0800

    add Question model

--- question/models.py
+++ question/models.py
@@ -21,3 +21,36 @@ class Tag(models.Model):
     history = HistoricalRecords()
 
 
+QUESTION_TYPE = (
+    (1, 'Simple Question'),
+    (2, 'Multiple Choice Question')
+)
+
+
+class Question(models.Model):
+    """Model to store question
+
+    """
+    hash_id = models.CharField(max_length=200, help_text='hashed id for the question')
+    title = models.CharField(null=True, blank=True, max_length=200, help_text='question title')
+    difficulty = models.IntegerField(null=True, blank=True, help_text='difficulty for the question')
+    source = models.CharField(null=True, blank=True, max_length=200, help_text='question source')
+    rating = models.IntegerField(null=True, blank=True, help_text='rating for the question')
+    type = models.SmallIntegerField(choices=QUESTION_TYPE, help_text='type of the question')
+    # owner of the question: TO be added later
+    is_public = models.BooleanField(default=True, help_text='whether this question is public or not')
+    original_body = models.TextField(null=True, blank=True, help_text='original question body,'
+                                                                      ' used for reference or non-parametric question')
+    original_image = models.ImageField(null=True, blank=True, help_text='image in original question')
+    original_answer = models.TextField(null=True, blank=True, help_text='original question answer, '
+                                                                        'used for reference or non-parametric question')
+    original_answer_image = models.ImageField(null=True, blank=True, help_text='image in original answer')
+    body = models.TextField(null=True, blank=True, help_text='parametric question body and answer stored as json,'
+                                                             ' null for non-parametric question')
+    body_image = models.ImageField(null=True, blank=True, help_text='image in body')
+    tags = models.ManyToManyField(Tag, null=True, blank=True, help_text='Tags that this question belong, 5 maximum')
+    creation_time = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    # track changes of data (for debugging or querying)
+    history = HistoricalRecords()
+
